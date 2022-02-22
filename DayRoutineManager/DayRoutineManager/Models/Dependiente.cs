281f11a489b592a872aaa30c516f34a14405c94d
Manuel <manuelperezp22@hotmail.com>
2022-02-22 12:45:03 -0600

    Creacion de clase dependientes

---
+++ DayRoutineManager/DayRoutineManager/Models/Dependiente.cs
@@ -0,0 +1,15 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DayRoutineManager.Models
+{
+    class Dependiente
+    {
+        [PrimaryKey]
+        public string id_dependiente { get; set; }
+        public string codigo_dependiente { get; set; }
+
+    }
+}
