541ad7605c86ced5ed57f043625187e9dbea2410
Raian M. Gil del Rosario <Raian.gdr@gmail.com>
2022-03-05 00:08:11 -0400

    Adding clicked function to buttons

--- DayRoutineManager/DayRoutineManager/Views/Login.xaml.cs
+++ DayRoutineManager/DayRoutineManager/Views/Login.xaml.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
-namespace DayRoutineManager
+namespace DayRoutineManager.Views
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class Login : ContentPage
@@ -17,8 +17,16 @@ namespace DayRoutineManager
         public Login()
         {
             InitializeComponent();
+        }
 
-           
+        private void btnJoinAdmin_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new Dashboard());
+        }
+
+        private void btnJoinDependiente_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new dependentDashboard());
         }
     }
 }
