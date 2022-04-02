c5ed982dccd8ce055c345ab18440c81fa44faa8f
Manuel Perez <manuelperezp22@hotmail.com>
2022-04-01 20:11:30 -0400

    update notification listo

--- DayRoutineManager/DayRoutineManager/Views/RecordatorioDeteail.xaml.cs
+++ DayRoutineManager/DayRoutineManager/Views/RecordatorioDeteail.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using DayRoutineManager.Models;
 using DayRoutineManager.Service;
+using Plugin.LocalNotification;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,6 +14,8 @@ namespace DayRoutineManager.Views
     public partial class RecordatorioDeteail : ContentPage
     {
         DBFirebase services;
+        Random rdn = new Random();
+        public string codigo_dependiente;
         public RecordatorioDeteail(RecortadorioModel recordatorio)
         {
             InitializeComponent();
@@ -24,12 +27,35 @@ namespace DayRoutineManager.Views
         {
             await services.UpdateRecordatorio(titulo_recordatorio.Text, descripcion_recordatorio.Text, DateTime.Parse(fecha_recordatorio.Text));
             await Navigation.PushAsync(new Recordatorios());
+            updateNotification();
+
         }
 
         public async void btnDelete_Clicked(object sender, EventArgs e)
         {
             await services.DeleteRecordatorio(titulo_recordatorio.Text, descripcion_recordatorio.Text, DateTime.Parse(fecha_recordatorio.Text));
             await Navigation.PushAsync(new Recordatorios());
+        }
+
+        private async void updateNotification()
+        {
+
+            int Next = rdn.Next();
+            Console.WriteLine(Next);
+            var notification = new NotificationRequest
+            {
+                BadgeNumber = 1,
+                Description = descripcion_recordatorio.Text,
+                Title = titulo_recordatorio.Text,
+                ReturningData = "Datos añadidos exitosamente!",
+                NotificationId = Next,
+                Schedule =
+                    {
+                        NotifyTime = DateTime.Parse(fecha_recordatorio.Text),
+                    }
+            };
+            NotificationCenter.Current.Show(notification);
+
         }
     }
 }
